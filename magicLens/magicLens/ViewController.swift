cc7700ab0a19f1ca604876b19040859c0506f213
Shahrez Jan <snjan19@bu.edu>
2016-10-21 20:38:42 -0400

    baby steps

---
+++ magicLens/magicLens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magicLens
+//
+//  Created by Shahrez Jan on 10/21/16.
+//  Copyright © 2016 Shahrez Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
